1c0df6e79b1b61cfd020a9ff9827f5ea47b06500
andikadwikidd <wiqy0106@gmail.com>
2023-05-30 17:03:12 +0700

    set up api gateaway

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { APP_NAME } = process.env;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("test " + APP_NAME);
 });
 
 module.exports = router;
