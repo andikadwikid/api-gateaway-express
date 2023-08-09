2ca7bb17b1717cc3c671642072beb8733950f459
andikadwikidd <wiqy0106@gmail.com>
2023-08-09 16:35:07 +0700

    integrate api gateaway [create media]

--- routes/media.js
+++ routes/media.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { APP_NAME } = process.env;
+
+const mediaHandler = require("./handler/media");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("media");
-});
+router.post("/", mediaHandler.create);
 
 module.exports = router;
